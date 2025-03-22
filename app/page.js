591990f753c6d34dd35b19cabfaf681ea421b49d
Antriksh Gwal <antrikshgwal@gmail.com>
2025-03-22 22:58:37 +0530

    User management initialised

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      CHOP SUEY!
+     <br/>
+     <Button>SOAD</Button>
+
+    </div>
+  );
+}
